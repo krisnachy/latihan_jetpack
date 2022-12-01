7aa06b66a82c97e401b7bc3a18d93a200498b1e7
krisnachy <cahyayuana@gmail.com>
2022-12-01 11:48:50 +0700

    update add column

--- app/src/main/java/com/krisnachy/latihanjetpack/MainActivity.kt
+++ app/src/main/java/com/krisnachy/latihanjetpack/MainActivity.kt
@@ -3,13 +3,21 @@ package com.krisnachy.latihanjetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.krisnachy.latihanjetpack.ui.theme.LatihanJetpackTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +39,50 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
+    //Jika tidak memakai kolom maka akan ditumpuk seperti ini
     Text(text = "Hello $name!")
+
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxSize()
+    ) {
+        Text(text = "Left Text", Modifier.background(Color.Yellow))
+        Text(
+            text = "Center Text", modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .background(Color.Yellow)
+        )
+        Text(
+            text = "Right Text", modifier = Modifier
+                .align(Alignment.End)
+                .background(Color.Yellow)
+        )
+        Text(
+            text = "Lainnya",
+            modifier = Modifier
+                .padding(vertical = 12.dp)
+                .background(color = Color.Gray)
+                .padding(12.dp)
+                .background(color = Color.Green)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            "Green", modifier = Modifier
+                .background(Color.Green)
+                .fillMaxWidth()
+                .weight(1f),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            "Red", modifier = Modifier
+                .background(Color.Red)
+                .fillMaxWidth()
+                .weight(1f),
+            textAlign = TextAlign.Center
+        )
+    }
 }
 
 @Preview(showBackground = true)
